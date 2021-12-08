1009a4bbc446d8bcf9241f17bfeaeb2b8d7805a8
pinchuk.v.a <vitaly.pin4uck@yandex.ru>
2021-12-08 23:49:23 +0300

    initial commit.Base webpack setup

---
+++ README.md
@@ -0,0 +1,8 @@
+Project coded by:  [Vitaly Pinchuk](https://www.linkedin.com/in/vitaly-pinchuk-845182200/).  
+
+
+## About  
+
+***
+API: Coincap  
+[Flickr](https://www.flickr.com/).  
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require("path")
+const {CleanWebpackPlugin} = require("clean-webpack-plugin")
+const HTMLWebpackPlugin = require("html-webpack-plugin")
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].[hash].js"
+  },
+  devServer:{
+    port:3000
+  },
+  plugins:[
+    new HTMLWebpackPlugin({template: "./src/index.html"}),
+    new CleanWebpackPlugin(),
+  ],
+  module: {
+    rules:[
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ["style-loader","css-loader","sass-loader"]
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        use: ["file-loader"]
+      },
+    ]
+  },
+}
